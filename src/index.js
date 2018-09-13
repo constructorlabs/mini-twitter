e670392c24155afc0feea0fa7c2817d4f7db1f3b
YelChristensen <yelena.christensen@gmail.com>
2018-09-13 12:24:15 +0100

    Disable tweet submit when it's too long

--- src/index.js
+++ src/index.js
@@ -3,14 +3,26 @@ document.querySelector('form').addEventListener('submit', e => {
   let newTweet = document.querySelector('#tweet').value;
   newTweet = `<p> ${newTweet} </p>`;
   const newTweetNode = document.createElement('div');
+
   newTweetNode.innerHTML = newTweet;
   const timelineNode = document.querySelector('.timeline');
   const refTweetNode = document.querySelector('.timeline div:first-child');
-  timelineNode.insertBefore(newTweetNode, refTweetNode);
-  document.querySelector('#tweet').value = '';
-  document.querySelector('#count').textContent = 0;
+ if (document.querySelector('#tweet').value.length <= 280) {
+    timelineNode.insertBefore(newTweetNode, refTweetNode);
+    document.querySelector('#tweet').value = '';
+    document.querySelector('#count').textContent = 0;
+    }
 });
+
+
 
 document.querySelector('#tweet').addEventListener('input', e => {
   document.querySelector('#count').textContent = e.target.value.length;
+  if (e.target.value.length > 280) {
+      let count = document.querySelector('#count');
+      count.style.color = "red";
+
+  } else {
+    count.style.color = "black";
+  }
 });
