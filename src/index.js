9494254762da0811c38248faed16f268ad96b06b
Joe Lamb <joe@elevenplustwo.co.uk>
2018-09-13 11:13:39 +0100

    add new tweet to top of timeline

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-
 document.querySelector('form').addEventListener('submit', e => {
-    e.preventDefault();
-    let newTweet= document.querySelector("#tweet").value;
-    newTweet = `<div class="tweet"><p> ${newTweet} </p></div>`;
-    console.log(newTweet);
-    
+  e.preventDefault();
+  let newTweet = document.querySelector('#tweet').value;
+  newTweet = `<p> ${newTweet} </p>`;
+  const newTweetNode = document.createElement('div');
+
+  newTweetNode.innerHTML = newTweet;
+  const timelineNode = document.querySelector('.timeline');
+  const refTweetNode = document.querySelector('.timeline div:first-child');
+  console.log(refTweetNode.textContent);
+
+  timelineNode.insertBefore(newTweetNode, refTweetNode);
 });
-
-
